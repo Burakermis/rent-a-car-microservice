241a033574a78b37c3877955dc6dde706ddaf443
Huseyin Burak Ermis <h.burak_ermis@hotmail.com>
2023-05-28 22:31:21 +0300

    added invoice service

---
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
@@ -0,0 +1,24 @@
+package com.kodlamaio.rentalservice.api.clients;
+
+import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import com.kodlamaio.commonpackage.utils.dto.GetCarResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class CarClientFallback implements CarClient {
+    @Override
+    public ClientResponse checkIfCarAvailable(UUID carId) {
+        log.info("INVENTORY SERVICE IS DOWN!");
+        throw new RuntimeException("INVENTORY SERVICE IS DOWN!");
+    }
+
+    @Override
+    public GetCarResponse getById(UUID carId) {
+        log.info("INVENTORY SERVICE IS DOWN!");
+        throw new RuntimeException("INVENTORY SERVICE IS DOWN!");
+    }
+}
